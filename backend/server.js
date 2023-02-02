d6e0de5b0df2ab86e1b933459a9f461345b64051
Trung Thanh Huynh <trungthanh.huynh@dxc.com>
2023-02-02 10:11:58 +0700

    Build API for my app

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,37 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const fileUpload = require("express-fileupload")
+const cookieParser = require("cookie-parser")
+const app = express();
+const port = 3000;
 
-const apiRoutes = require("./routes/apiRoutes")
+app.use(express.json())
+app.use(cookieParser())
+app.use(fileUpload())
 
-app.get('/', (req, res) => {
-    console.log("synchronous code")
-    throw new Error("some error occured")
-    res.json({message: "API running..."})
-})
+const apiRoutes = require("./routes/apiRoutes");
 
-app.get('/a', (req,res,next) => {
-    setTimeout(() => {
-        try {
-            aconsole.log("asynchronouse code");
-        } catch (er) {
-            next(er);
-        }
-    },1000)
-    // next(new Error("some error occured"));
-})
+app.get("/", async (req, res, next) => {
+  res.json({ message: "API running..." });
+});
 
-app.use('/api', apiRoutes)
+// mongodb connection
+const connectDB = require("./config/db");
+connectDB();
+
+app.use("/api", apiRoutes);
 
 app.use((error, req, res, next) => {
-    console.error(error);
-    next(error)
-})
+  console.error(error);
+  next(error);
+});
 app.use((error, req, res, next) => {
-    res.status(500).json({
-        message: error.message,
-        stack: error.stack
-    })
-})
+  res.status(500).json({
+    message: error.message,
+    stack: error.stack,
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
+
